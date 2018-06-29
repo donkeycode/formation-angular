5e15a0e664ffc19864ba6705e955fc96fc0126ef
Raphael Yan <raphael@donkeycode.com>
2018-06-29 16:48:22 +0200

    ng-template & ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   cats = [
-    'https://www.chewy.com/petcentral/wp-content/uploads/2018/05/abyssinian-cat-card.jpg',
-    'https://www.argospetinsurance.co.uk/assets/uploads/2017/12/cat-pet-animal-domestic-104827.jpeg',
-    'https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg'
+    {
+      url: 'https://www.chewy.com/petcentral/wp-content/uploads/2018/05/abyssinian-cat-card.jpg',
+      name: 'chat mignon'
+    },
+    {
+      url: 'https://www.argospetinsurance.co.uk/assets/uploads/2017/12/cat-pet-animal-domestic-104827.jpeg',
+      name: 'chat encore mignon'
+    },
+    {
+      url: 'https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg',
+      name: 'chat toujours mignon'
+    },
   ];
 
   imageClicked(event) {

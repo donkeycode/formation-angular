1133c3a679b73dd1b7b1368890fa7bcee8101ddf
Raphael Yan <raphael@donkeycode.com>
2018-06-29 18:37:31 +0200

    http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -23,6 +24,14 @@ export class AppComponent {
       name: 'chat toujours mignon'
     },
   ];
+  dog;
+
+  constructor(private http: HttpClient) {
+    this.http.get('https://dog.ceo/api/breeds/image/random')
+    .subscribe((response) => {
+      this.dog = response['message'];
+    });
+  }
 
   imageClicked(event) {
     console.log(event);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ColorDirective } from './color.directive';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +20,8 @@ import { MatCardModule } from '@angular/material/card';
     BrowserModule,
     MatButtonModule,
     MatCheckboxModule,
-    MatCardModule
+    MatCardModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
